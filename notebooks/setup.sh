2510bf27e52126506950f8868181fefc8f8e6045
harshulgupt <harshulgupt@gmail.com>
2023-05-29 17:27:35 +0530

    setup-cleanup bash scripts

---
+++ notebooks/setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd ../../group-data
+mkdir members/common
+mkdir website_data/research/sub_research_data/combined
+
+cd ..
+cd kerzendorf-group.github.io
+rm -rf index.html Research.html Contact.html People.html Support.html News.html
+rm -rf assets website_files members sub_research support_images news
+mkdir members sub_research news
+cp -r ../group-data/website_data/website_files ./
+cp -r ../group-data/website_data/support_images ./
+cp -r ../groupwebsite_generator/assets ./
+rsync -av --exclude '*.json' --exclude 'jsons' ../group-data/members/ ./members
