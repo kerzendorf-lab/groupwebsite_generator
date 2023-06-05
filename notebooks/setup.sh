d715d857d037db8b4038dd759c12efea727a9890
harshulgupt <harshulgupt@gmail.com>
2023-06-05 23:48:18 +0530

    Research section issues

--- notebooks/setup.sh
+++ notebooks/setup.sh
@@ -11,5 +11,7 @@ rm -rf assets website_files members sub_research support_images news
 mkdir members sub_research news
 cp -r ../group-data/website_data/website_files ./
 cp -r ../group-data/website_data/support_images ./
+cp -r ../group-data/website_data/news/images ./news/
+
 cp -r ../groupwebsite_generator/assets ./
 rsync -av --exclude '*.json' --exclude 'jsons' ../group-data/members/ ./members
