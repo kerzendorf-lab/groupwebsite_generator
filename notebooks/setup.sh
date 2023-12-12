7248a1d66caa0e09bcb0cb4e35a3b1d1692159e2
KasukabeDefenceForce <abhinavohri13@gmail.com>
2023-12-12 18:25:02 +0530

    Website deployment. (#73)
    * Try publishing to github pages
    
    * debug notebook run
    
    * Cleanup setup.sh and use it in Github actions as well
    
    * Removed personal repository links
    
    * Refactor workflow files for better event handling
    
    * Refactor website update workflow.
    
    * Apply suggestions from code review
    
    Co-authored-by: Atharva Arya <55894364+atharva-2001@users.noreply.github.com>
    
    * remove unneeded steps and reorder steps
    
    * Use working-directory instead of cd
    
    * Clone repos before python setup
    
    * Use TARDIS_BOT_TOKEN for PAT
    
    * Use github-actions[bot] for website update commits
    
    ---------
    
    Co-authored-by: Atharva Arya <55894364+atharva-2001@users.noreply.github.com>

--- notebooks/setup.sh
+++ notebooks/setup.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
 
-cd ../../group-data
-mkdir members
-mkdir temp
-mkdir temp/members
-mkdir website_data/research/sub_research_data/combined
-
-cd ..
-cd kerzendorf-group.github.io
+cd "$(dirname "${BASH_SOURCE[0]}")/../../kerzendorf-group.github.io"
 rm -rf index.html Research.html Contact.html People.html Support.html News.html
 rm -rf assets website_files members sub_research support_images news
-mkdir members sub_research news
-cp -r ../group-data/website_data/website_files ./
-cp -r ../group-data/website_data/support_images ./
-cp -r ../group-data/website_data/news/images ./news/
 
+mkdir members sub_research news
+
+cp -r ../group-data/website_data/website_files ./
 cp -r ../groupwebsite_generator/assets ./
+
 rsync -av --exclude '*.json' --exclude 'jsons' ../group-data/members/ ./members
